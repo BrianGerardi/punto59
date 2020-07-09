e91c5f90cabf3be2a02f03798d11b2a848f1ba03
Gregorio Gerardi <Gregorio.Gerardi@ibm.com>
2020-07-08 22:58:16 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/punto59/precache-manifest.d766dd4a33fb605d266778c79850686f.js"
+  "/punto59/precache-manifest.3f90cf1513c4cb9d991cf7b77363b875.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/punto59/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

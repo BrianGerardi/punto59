0d5df2722a53a549ba587d46a1ca1561dd217258
BrianGerardi <briangerardi48@gmail.com>
2020-07-13 14:46:36 -0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import Button from "@material-ui/core/Button"
 
+import { ThemeProvider } from "@material-ui/core/styles"
+import theme from "./temaConfig"
+
+import Navbar from "./components/Navbar"
+import { Typography } from '@material-ui/core';
+
 function App() {
   return (
-    <div><h1>hola</h1>
-    <Button>asd</Button></div>
-    
+    <ThemeProvider theme={theme}>
+      <Navbar></Navbar>
+      {/*<Button variant="contained" color="primary">boton 1</Button>
+      <Button variant="contained" color="secondary">boton 2</Button>*/}
+      <Typography variant="h1">texto de prueba</Typography>
+    </ThemeProvider>
+
   )
-  }
+}
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+
+import React from "react"
+import { AppBar, Typography, Toolbar, makeStyles, IconButton, Button, } from "@material-ui/core"
+const useStyles = makeStyles(theme => ({
+    offset: theme.mixins.toolbar,
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1
+    }
+
+
+}))
+const Navbar = () => {
+    const classes = useStyles()
+    return (
+        <div>
+            <AppBar>
+                <Toolbar>
+                    <IconButton color="inherit" aria-label="menu1" ClassName=
+                        {classes.menuButton}></IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                        Punto 59
+                </Typography>
+                    <Button variant="text" color="secondary">
+                        Menu 1
+                </Button>
+                    <Button variant="text" color="secondary">
+                        Menu 2
+                </Button>
+                    <Button variant="text" color="secondary">
+                        Menu 3
+                </Button>
+                    <Button variant="text" color="secondary">
+                        Menu 4
+                </Button>
+                    <Button variant="text" color="secondary">
+                        Menu 5
+                </Button>
+                </Toolbar>
+            </AppBar>
+            <div className={classes.offset}></div>
+
+        </div>
+    )
+}
+export default Navbar

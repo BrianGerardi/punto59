53cb907a8740a77103a40b7bffd3d71e9865e530
BrianGerardi <briangerardi48@gmail.com>
2020-07-14 06:02:04 -0300

    Punto59-8-navigationdone

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
-import Button from "@material-ui/core/Button"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  //Link,
+  NavLink,
+} from "react-router-dom"
+import Inicio from './Components/inicio';
+import Nosotros from './Components/nosotros';
+import Contacto from './Components/contacto';
 
 function App() {
   return (
-    <div><h1>hola</h1>
-    <Button>asd</Button></div>
-    
+    <Router>
+      <div className="container mt-5">
+        <div className= "btn-group">
+          <NavLink to="/" exact className="btn-dark" activeClassName="active">Inicio</NavLink>
+          <NavLink to="/Nosotros" exact className="btn-dark" activeClassName="active">Nosotros</NavLink>
+          <NavLink to="/Contacto" exact className="btn-dark" activeClassName="active">Contacto</NavLink>
+        </div>
+        <hr />
+        <Switch>
+          <Route path="/Nosotros">
+            <Nosotros />
+          </Route>
+          <Route path="/Contacto">
+            <Contacto />
+          </Route>
+          <Route path="/" exact>
+            <Inicio />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+
   )
-  }
+}
 export default App;
